d4ed17a8e3b5ce69e9363ab544afadcf3f3a14b9
Victoria Banjo <67071771+banjovic@users.noreply.github.com>
2023-07-22 09:07:43 +0100

    updates on styling

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -7,30 +7,43 @@ import img8 from '../assets/images/prodImg6.png';
 const Featured = () => {
   return (
     <div>
-      <div className="featured">
+      <div className="featured-section">
         <div className="fey">
           <h1>FEATURED</h1>
         </div>
+
         <div className="img-section2">
           <div className="manual">
-            <img width="400px" height="500px" src={img4} alt="" />
-            <h1 className="manu">Flowers on Sleeves Dress</h1>
-            <h2 className="manua">$115</h2>
+            <img src={img4} alt="" />
+
+            <div>
+              <h1>Flowers on Sleeves Dress</h1>
+              <h2>$115</h2>
+            </div>
           </div>
-          <div className="manual1">
-            <img width="400px" height="500px" src={img5} alt="" />
-            <h1 className="manu">Cat-Eye Resin Sunglasses in Clear</h1>
-            <h2 className="manua">$45</h2>
+          <div className="manual">
+            <img src={img5} alt="" />
+
+            <div>
+              <h1>Cat-Eye Resin Sunglasses in Clear</h1>
+              <h2>$45</h2>
+            </div>
           </div>
-          <div className="manual2">
-            <img width="400px" height="500px" src={img8} alt="" />
-            <h1 className="manu">Notte Dot Dress</h1>
-            <h2 className="manua">$74</h2>
+          <div className="manual">
+            <img src={img8} alt="" />
+
+            <div>
+              <h1>Notte Dot Dress</h1>
+              <h2>$74</h2>
+            </div>
           </div>
-          <div className="manual3">
-            <img width="400px" height="500px" src={img6} alt="" />
-            <h1 className="manu">Marigold Trouser</h1>
-            <h2 className="manua">$78</h2>
+          <div className="manual">
+            <img src={img6} alt="" />
+
+            <div>
+              <h1>Marigold Trouser</h1>
+              <h2>$78</h2>
+            </div>
           </div>
         </div>
       </div>
